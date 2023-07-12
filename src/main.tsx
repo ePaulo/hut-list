86b5868959d05876b20ffff244af6c0c61e710bd
ePaulo <paul.olynek@live.ca>
2023-07-12 07:57:16 -0700

    initial commit includes disabled eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+// import './index.css'
 
-import App from './App';
-
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-    <StrictMode>
-        <App />
-    </StrictMode>
-);
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
